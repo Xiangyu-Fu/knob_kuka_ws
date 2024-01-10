dd7a830d533988fc719ab453b5a29c29f493a8a7
Xiangyu Fu <fu@fortiss.org>
2024-01-10 17:34:00 +0100

    Add one way position controller.

--- src/knob_robot_control/scripts/knob_ony_way_position_controller.py
+++ src/knob_robot_control/scripts/knob_ony_way_position_controller.py
@@ -4,12 +4,13 @@
 # Description: This script is used to control the robot arm through the robot movement interface.
 
 import rospy
-from robot_movement_interface.msg import Command, CommandList
+from robot_movement_interface.msg import Command, CommandList, EulerFrame
 from robot_movement_interface.msg import Result
 from sensor_msgs.msg import JointState
 from std_msgs.msg import String, Int32, Float32
 from knob_robot_control.msg import KnobState
 from device_msgs.msg import Status
+from geometry_msgs.msg import WrenchStamped
 from threading import Lock
 
 
@@ -26,17 +27,56 @@ class RobotController:
         # rospy.Subscriber("/command_result", Result, self.command_result_callback)
 
         self.iiwa_driver_command_list = rospy.Publisher("/command_list", CommandList, queue_size=2)
-        self.iiwa_driver_command_result = rospy.Subscriber("/command_result", Result, self.command_result_callback)
-        self.iiwa_driver_joint_states = rospy.Subscriber("/joint_states", JointState, self.joint_states_callback)
+        #self.iiwa_driver_command_result = rospy.Subscriber("/command_result", Result, self.command_result_callback)
+        #self.iiwa_driver_joint_states = rospy.Subscriber("/joint_states", JointState, self.joint_states_callback)
+
+        self.robot_tcp_state_subscriber = rospy.Subscriber("/tool_frame", EulerFrame, self.tool_frame_subscribe_callback)
+        self.robot_tcp_state_subscriber = rospy.Subscriber("/tcp_wrench", WrenchStamped, self.tcp_wrench_subscribe_callback)
+
+        self.knob_state_subscriber = rospy.Subscriber("/knob_state", KnobState, self.knob_state_callback)
 
         self.currentCommandId = 0
 
         self.mStatus = Status()
         self.mStatusMutex = Lock()
 
-    def joint_states_callback(self, data) -> None:
-        # rospy.loginfo("Joint states received: ...")  
-        pass
+        self.robot_tcp_position_current = [0, 0, 0]
+        self.robot_tcp_orientation_current = [0, 0, 0]
+        self.robot_tcp_force_current = [0, 0, 0]
+
+        self.robot_tcp_position_target = [0, 0, 0]
+        self.robot_tcp_orientation_target = [0, 0, 0]
+        self.robot_tcp_force_threshold = [5, 5, 5]
+
+        self.knob_force = 0
+        self.knob_position = 0
+        self.knob_position_delta = 0
+
+        self.position_factor = 0.01
+        self.force_factor = 1
+        self.velocity_factor = 1
+
+        self.ROBOT_STATE_INITIALIZED = False
+
+    def knob_state_callback(self, data):
+        self.knob_position_delta = data.position.data - self.knob_position
+        self.knob_position = data.position.data
+        self.knob_force = data.force.data
+
+        #print("self.knob_position_delta, self.knob_position, self.knob_force", self.knob_position_delta, self.knob_position, self.knob_force)
+
+    def tool_frame_subscribe_callback(self, data) -> None:
+        self.robot_tcp_position_current = [data.x+0.004, data.y - 0.005, data.z-0.03]
+        self.robot_tcp_orientation_current = [data.alpha, data.beta, data.gamma]
+
+        #print('self.robot_tcp_position_current, self.robot_tcp_orientation_current', self.robot_tcp_position_current, self.robot_tcp_orientation_current)
+
+        self.ROBOT_STATE_INITIALIZED = True
+        
+    def tcp_wrench_subscribe_callback(self, data) -> None:
+        self.robot_tcp_force_current = [data.wrench.force.x, data.wrench.force.y, data.wrench.force.z]
+
+        #print('self.robot_tcp_force_current', self.robot_tcp_force_current)
         
     def send_command_to_robot(self, commands, replace_previous=True) -> None:
         """
@@ -229,21 +269,17 @@ class RobotController:
         return True
 
     def run(self) -> None:
-        feq = 1
+        feq = 100
         rate = rospy.Rate(feq) # 10hz
         
         # Sample data for testing
-        position_target = [-0.08374, 0.5547, 0.05418]
-        position_home = [-0.08374, 0.4547, 0.25418]
-        angles = [0.0, -0.0, 3.14]
-        joints = [1.5710205516437281, 0.26206090357094514, -2.6964464278686393e-05, -1.2529596421209066, 7.200128936299848e-05, 1.6281237054938813, -1.570994186372798]
-        force = [5, 5, 5]
-
-        count = 0
-
-        rospy.sleep(3)
+        # position_target = [-0.08374, 0.5547, 0.05418]
+        # position_home = [-0.08374, 0.4547, 0.25418]
+        # angles = [0.0, -0.0, 3.14]
+        # joints = [1.5710205516437281, 0.26206090357094514, -2.6964464278686393e-05, -1.2529596421209066, 7.200128936299848e-05, 1.6281237054938813, -1.570994186372798]
+        # force = [5, 5, 5]
         
-        while not rospy.is_shutdown():
+        #while not rospy.is_shutdown():
             # if count == 2:
             #     self.sendMoveCartesianLinForce(position, angles, force)
             # Test sendMoveCartesianLin
@@ -261,15 +297,29 @@ class RobotController:
             #     position_new = [-0.08374, 0.5547, 0.23418]
             # position_new[2] = position_new[2] - 0.09
 
-            self.sendMoveCartesianLinImpedence(position_target, angles, force)
+            #self.sendMoveCartesianLinImpedence(position_target, angles, force)
             # count += 1
             # rate.sleep()
 
-            rospy.sleep(10)
-            self.sendMoveCartesianLinImpedence(position_home, angles, force)
-            rospy.sleep(10)
+            #rospy.sleep(10)
+            #self.sendMoveCartesianLinImpedence(position_home, angles, force)
+            #rospy.sleep(10)
         
-        # rospy.spin()
+        #rospy.spin()
+
+        while not rospy.is_shutdown():
+            if self.knob_position_delta != 0 and self.ROBOT_STATE_INITIALIZED:
+                print('knob position delta:',  self.knob_position_delta)
+                print('robot_tcp_position_current:',  self.robot_tcp_position_current)
+                position_change = self.position_factor * self.knob_position_delta
+                print("Posiiton change,", position_change)
+                self.robot_tcp_position_target[0] = self.robot_tcp_position_current[0] + position_change
+                self.robot_tcp_position_target[1] = self.robot_tcp_position_current[1]
+                self.robot_tcp_position_target[2] = self.robot_tcp_position_current[2]
+                print('robot_tcp_position_target:',  self.robot_tcp_position_target)
+                
+                #self.sendMoveCartesianLin(self.robot_tcp_position_current, self.robot_tcp_orientation_current)
+                self.sendMoveCartesianLinImpedence(self.robot_tcp_position_current, self.robot_tcp_orientation_current, self.robot_tcp_force_threshold)
 
 if __name__ == '__main__':
     controller = RobotController()
